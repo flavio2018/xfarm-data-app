953f314c4a82f6c5d161ccfc72be50dc1b93b85a
flavio2018 <flavio.petruzzellis@gmail.com>
2021-08-28 18:11:57 +0200

    Change sensors keys

--- example.py
+++ example.py
@@ -1,17 +1,25 @@
+from FarmOS import FarmOS
+from Sensor import Sensor
+
 if __name__ == '__main__':
 
     farmos = FarmOS('192.168.8.106')
 
-    res = farmos.get_logs({'type': 'soil_test',
-                           'log_category': 'soil'})
+    res = farmos.get(
+        resource='log',
+        filters={'type': 'purchase',
+                 'log_category': "Costi%20Produzione%20Olio",
+                 }
+    )
+    print(res)
 
     temp_sensor = Sensor(
-        name='temperature',
-        public_key='e0c7f449a0640908ce89697038d93c58',
-        private_key='50b44cade7133104cea6692042533410'
+        name='temperature0',
+        public_key='2af62b0d5189b65e7fb8bf7024fb574e',
+        private_key='76102647a4127eada9aac3bae1229519'
     )
 
     farmos.add_sensor(temp_sensor)
-    res = farmos.sensors['temperature'].get_values()
+    res = farmos.sensors['temperature0'].get_values()
 
     print(res)
